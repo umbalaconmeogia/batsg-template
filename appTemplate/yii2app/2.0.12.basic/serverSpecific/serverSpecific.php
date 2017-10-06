c16dd895fcc3279a687f1068874b2598d1b1d39b
Tran Trung Thanh <umbalaconmeogia@gmail.com>
2017-10-06 14:47:20 +0900

    Change db dns to sqlite.

--- appTemplate/yii2app/2.0.12.basic/serverSpecific/serverSpecific.php
+++ appTemplate/yii2app/2.0.12.basic/serverSpecific/serverSpecific.php
@@ -1,9 +1,9 @@
 <?php
-$cache = TRUE;
+$cache = FALSE;
 return [
 	'db' => [
 		'class' => 'yii\db\Connection',
-		'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+		'dsn' => 'sqlite:@app/../serverSpecific/data/db.sqlite',
 		'username' => 'root',
 		'password' => '',
 		'charset' => 'utf8',
