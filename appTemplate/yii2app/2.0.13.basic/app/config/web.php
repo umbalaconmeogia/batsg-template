3e4731eaaac061f2e9662901a807c75b72f116f7
Tran Trung Thanh <umbalaconmeogia@gmail.com>
2017-12-03 20:33:45 +0900

    Move vendor into yii2

--- appTemplate/yii2app/2.0.13.basic/app/config/web.php
+++ appTemplate/yii2app/2.0.13.basic/app/config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'basic-dfoi23498h3f4',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
